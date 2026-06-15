bb98ea482fa369c3ce7b3a3f3b49a968031e762c
Abdullah <abdullahkhan221121@gmail.com>
2026-06-15 18:54:31 +0500

    fifth commit

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,23 +1,85 @@
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker, declarative_base
+
+# from app.core.config import (
+#     DB_SERVER,
+#     DB_NAME,
+#     DB_DRIVER
+# )
+
+
+# # ==========================
+# # DATABASE CONNECTION
+# # WINDOWS AUTHENTICATION
+# # ==========================
+
+# DATABASE_URL = (
+#     f"mssql+pyodbc://@{DB_SERVER}/{DB_NAME}"
+#     f"?driver={DB_DRIVER.replace(' ', '+')}"
+#     "&trusted_connection=yes"
+# )
+
+
+# engine = create_engine(
+#     DATABASE_URL,
+#     echo=True
+# )
+
+
+# # ==========================
+# # SESSION
+# # ==========================
+
+# SessionLocal = sessionmaker(
+#     autocommit=False,
+#     autoflush=False,
+#     bind=engine
+# )
+
+
+# # ==========================
+# # BASE MODEL
+# # ==========================
+
+# Base = declarative_base()
+
+
+
+# # ==========================
+# # DATABASE DEPENDENCY
+# # ==========================
+
+# def get_db():
+
+#     db = SessionLocal()
+
+#     try:
+#         yield db
+
+#     finally:
+#         db.close()
+
+# aptech system
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 from app.core.config import (
     DB_SERVER,
     DB_NAME,
+    DB_USER,
+    DB_PASSWORD,
     DB_DRIVER
 )
 
 
-# ==========================
-# DATABASE CONNECTION
-# WINDOWS AUTHENTICATION
-# ==========================
 
 DATABASE_URL = (
-    f"mssql+pyodbc://@{DB_SERVER}/{DB_NAME}"
+    f"mssql+pyodbc://{DB_USER}:{DB_PASSWORD}@{DB_SERVER}/{DB_NAME}"
     f"?driver={DB_DRIVER.replace(' ', '+')}"
-    "&trusted_connection=yes"
+    "&trusted_connection=no"
 )
+
 
 
 engine = create_engine(
@@ -26,9 +88,6 @@ engine = create_engine(
 )
 
 
-# ==========================
-# SESSION
-# ==========================
 
 SessionLocal = sessionmaker(
     autocommit=False,
@@ -37,24 +96,19 @@ SessionLocal = sessionmaker(
 )
 
 
-# ==========================
-# BASE MODEL
-# ==========================
 
 Base = declarative_base()
 
 
-
-# ==========================
-# DATABASE DEPENDENCY
-# ==========================
 
 def get_db():
 
     db = SessionLocal()
 
     try:
+
         yield db
 
     finally:
+
         db.close()
